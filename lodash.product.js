f82fce052a1279e46883394373acd3cdf9bac633
Sergej Sintschilin <seregpie@gmail.com>
2017-06-27 10:23:32 +0200

    Add files via upload

---
+++ lodash.product.js
@@ -0,0 +1,24 @@
+(function(_) {
+
+	_.mixin({product: function() {
+		var collections = _.map(arguments, _.values);
+		var product = [];
+		(function f(tuple) {
+			if (tuple.length < collections.length) {
+				_.forEach(collections[tuple.length], function(value) {
+					f(_.concat(tuple, [value]));
+				});
+			} else {
+				product.push(tuple);
+			}
+		})([]);
+		return product;
+	}});
+
+})((function() {
+	if (typeof exports !== 'undefined' && this === exports) {
+		return require('lodash');
+	} else {
+		return _;
+	}
+}).call(this));

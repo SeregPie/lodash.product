72f68186deee744e6b1097d92349774924145635
Sergej Sintschilin <seregpie@gmail.com>
2018-09-19 17:37:48 +0200

    update test

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
 /*eslint no-console: 0*/
 
-
-const _ = require('lodash');
 require('./lodash.product');
+let _ = require('lodash');
 
 {
 	let product = _.product([false, true], ['a', 'b', 'c'], [{}]);
-	console.log(product);
+	console.log(_.isEqual(product, [[false, 'a', {}], [false, 'b', {}], [false, 'c', {}], [true, 'a', {}], [true, 'b', {}], [true, 'c', {}]]));
 }
 {
 	let array = [1, 2, 3];
 	let product = _.product(array, array);
-	console.log(product);
+	console.log(_.isEqual(product,  [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]));
+}
+{
+	let product = _.product([1, 2]);
+	console.log(_.isEqual(product, [[1], [2]]));
+}
+{
+	let product = _.product([], [], []);
+	console.log(_.isEqual(product, []));
 }
